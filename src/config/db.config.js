70e72a665e12f50c524a47d3149dd5c200f376ce
Dhairya99999 <110965688+Dhairya99999@users.noreply.github.com>
2024-04-24 20:10:42 +0530

    Add files via upload

---
+++ src/config/db.config.js
@@ -0,0 +1,19 @@
+
+
+import mongoose from 'mongoose';
+
+const url = "mongodb://localhost:27017";
+
+export const connectUsingMongoose = async()=>{
+   
+   
+   try{ await mongoose.connect(`${url}/chatterUp`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log("MongoDB connected using mongoose");
+} catch (err) {
+    console.log(err);
+}
+
+}
